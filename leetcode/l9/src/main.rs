956aa88da575bd062e294d7e45f3f2f62a4f6cf4
Tozinoo <goldboy122@gmail.com>
2024-02-05 16:07:29 +0900

    coding test leetcode 9

---
+++ leetcode/l9/src/main.rs
@@ -0,0 +1,22 @@
+
+fn main() {
+    let result = is_palindrome(4001);
+    println!("{}",result);
+}
+
+pub fn is_palindrome(x: i32) -> bool {
+    if x<0 {
+        return false
+    } else  {
+        let temp = x.to_string();
+        let num = temp.len()/2;
+        let s = &temp[..num];
+        let back = &temp[temp.len()-num..];
+        let t:String = back.chars().rev().collect();
+        if s == t {
+            return true
+        } else {
+            return false
+        }
+    }
+}
